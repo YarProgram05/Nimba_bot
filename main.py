556d4952fe034cf0583fcb6cb90cbae44388f242
YarProgram05 <yaros-05@list.ru>
2025-10-07 01:13:07 +0300

    Nimba Program bot - version 0.17.9 Обновление. Остатки товаров Озон - автоматическая выгрузка отчета. И fix багов_12

--- main.py
+++ main.py
@@ -79,11 +79,11 @@ logger = logging.getLogger(__name__)
 def get_next_monday_10am(tz):
     """Возвращает datetime ближайшего понедельника в 10:00 по указанному часовому поясу"""
     now = datetime.datetime.now(tz)
-    days_ahead = (0 - now.weekday()) % 7  # 0 = понедельник
-    next_monday = now.replace(hour=21, minute=50, second=0, microsecond=0) + datetime.timedelta(days=days_ahead)
+    days_ahead = (1 - now.weekday()) % 7  # 0 = понедельник
+    next_monday = now.replace(hour=1, minute=15, second=0, microsecond=0) + datetime.timedelta(days=days_ahead)
 
     # Если сегодня понедельник, но уже после 10:00 — берём следующий понедельник
-    if days_ahead == 0 and now.time() > datetime.time(21, 50):
+    if days_ahead == 0 and now.time() > datetime.time(1, 15):
         next_monday += datetime.timedelta(weeks=1)
 
     return next_monday
